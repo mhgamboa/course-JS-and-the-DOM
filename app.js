3154f7d1a738583a2b8551f0dde93573d87d9e0f
Marcus <marcus.gamboa@gmail.com>
2020-09-30 11:10:59 -0600

    tested eventlistener

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-const mainHeading = document.querySelector("#main-heading");
-const otherHeading = document.querySelector("#other-heading");
-const excitedText = document.createElement("span");
-
-excitedText.textContent = "!!!";
-mainHeading.appendChild(excitedText);
-otherHeading.appendChild(excitedText);
+document.addEventListener("click", function () {
+  console.log("hi");
+});
